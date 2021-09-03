0b7e6e9c7e8ea778fbc268fd4adc4b8688c62192
drumath <2237k.drumath@gmail.com>
2021-09-04 02:56:52 +0900

    :sparkles: f32->f64への変更とテストの追加

--- src/vector3.rs
+++ src/vector3.rs
@@ -2,19 +2,33 @@ use std::ops;
 
 #[derive(Debug)]
 pub struct Vector3 {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
 impl Vector3 {
-    pub fn new(_x: f32, _y: f32, _z: f32) -> Vector3 {
+    pub fn new(_x: f64, _y: f64, _z: f64) -> Vector3 {
         return Vector3 {
             x: _x,
             y: _y,
             z: _z,
         };
     }
+
+    pub fn zero() -> Vector3 {
+        return Vector3::new(0.0, 0.0, 0.0);
+    }
+
+    pub fn normalize(v: Vector3) -> Vector3 {
+        todo!()
+    }
+
+    pub fn length(&self) -> f64 { todo!(); }
+
+    pub fn dot(v1: Vector3, v2: Vector3) -> f64 { todo!(); }
+
+    pub fn cross(v1: Vector3, v2: Vector3) -> Vector3 { todo!() }
 }
 
 impl ops::Add<Vector3> for Vector3 {
@@ -29,7 +43,7 @@ impl ops::Add<Vector3> for Vector3 {
     }
 }
 
-impl ops::Sub<Vector3> for Vector3{
+impl ops::Sub<Vector3> for Vector3 {
     type Output = Vector3;
 
     fn sub(self, rhs: Vector3) -> Self::Output {
@@ -49,6 +63,7 @@ impl std::cmp::PartialEq for Vector3 {
 #[cfg(test)]
 mod vector_test {
     use crate::vector3::Vector3;
+    use std::num::*;
 
     #[test]
     fn vector_new_test() {
@@ -69,6 +84,44 @@ mod vector_test {
         let v2 = Vector3::new(1.0, 0.0, 0.0);
 
         assert_eq!(v1 - v2, Vector3::new(0.0, 2.0, 3.0));
+    }
+
+    #[test]
+    fn zero_test() {
+        assert_eq!(Vector3::zero(), Vector3 { x: 0.0, y: 0.0, z: 0.0 });
+    }
+
+    #[test]
+    fn length_test() {
+        let vec = Vector3::zero();
+        assert_eq!(vec.length(), 0.0);
+
+        let vec = Vector3::new(1.0, 0.0, 0.0);
+
+        assert_eq!(vec.length(), 1.0);
+    }
+
+    #[test]
+    fn normalize_test() {
+        let vec = Vector3::new(1.0, 1.0, 1.0);
+        assert_eq!(Vector3::normalize(vec),
+                   Vector3::new((3.0 as f64).sqrt(), (3.0 as f64).sqrt(), (3.0 as f64).sqrt()))
+    }
+
+    #[test]
+    fn dot_test() {
+        let v1 = Vector3::new(1.0, 2.0, 3.0);
+        let v2 = Vector3::new(2.0, 3.0, 4.0);
+
+        assert_eq!(Vector3::dot(v1, v2), 2.0 + 6.0 + 12.0);
+    }
+
+    #[test]
+    fn cross_test() {
+        let v1 = Vector3::new(1.0, 2.0, 3.0);
+        let v2 = Vector3::new(4.0, 5.0, 6.0);
+
+        assert_eq!(Vector3::cross(v1, v2), Vector3::new(-3.0, 6.0, -3.0));
     }
 }
 
