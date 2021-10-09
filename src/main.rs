463868389e565817784ba81aa9f821dc343e0bb2
drumath <2237k.drumath@gmail.com>
2021-10-09 18:17:47 +0900

    :sparkles: the CGって感じの画像を書き出す遊び

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,18 @@ mod directionalLight;
 use crate::ray::Ray;
 
 fn main() {
+    let width = 64;
+    let height = 64;
+
     let mut img: RgbImage = ImageBuffer::new(64, 64);
 
-    for x in 0..32 {
-        for y in 0..32 {
-            img.put_pixel(x, y, Rgb([255, 0, 255]))
+    for y in 0..height {
+        for x in 0..width {
+            img.put_pixel(x, y, Rgb([
+                (255 * x / width) as u8,
+                (255 * y / height) as u8,
+                0
+            ]))
         }
     }
 
